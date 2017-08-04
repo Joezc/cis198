f6467d5b7fad6acaad73494a05601c9665894347
Joezc <lzc814310057@163.com>
2017-08-04 22:02:03 +0800

    implement simple hello echo function

--- hw07/src/chatserver.rs
+++ hw07/src/chatserver.rs
@@ -1,5 +1,9 @@
+extern crate websocket;
 use std::thread;
-
+use websocket::OwnedMessage;
+use websocket::sync::Server;
+//use websocket::server::sync::Request;
+use websocket::server::upgrade::WsUpgrade;
 const WS_ADDR: &'static str = "0.0.0.0:1981";
 
 #[derive(Debug, Clone)]
@@ -22,12 +26,49 @@ pub fn start() {
 /// relay thread, then listen for WebSocket clients and spawn their threads.
 fn listen() {
     // TODO
+    let server = Server::bind(WS_ADDR).unwrap();
+    println!("ChatServer listening on {}", WS_ADDR);
+    for request in server.filter_map(Result::ok) {
+        // Spawn a client_thread.
+        let mut client = request.accept().unwrap();
+
+        let ip = client.peer_addr().unwrap();
+
+        println!("Connection from {}", ip);
+
+        let message = OwnedMessage::Text("Hello".to_string());
+        client.send_message(&message).unwrap();
+
+        let (mut receiver, mut sender) = client.split().unwrap();
+
+        for message in receiver.incoming_messages() {
+            let message = message.unwrap();
+
+            match message {
+                OwnedMessage::Close(_) => {
+                    let message = OwnedMessage::Close(None);
+                    sender.send_message(&message).unwrap();
+                    println!("Client {} disconnected", ip);
+                    return;
+                }
+                OwnedMessage::Ping(ping) => {
+                    let message = OwnedMessage::Pong(ping);
+                    sender.send_message(&message).unwrap();
+                }
+                _ => sender.send_message(&message).unwrap(),
+            }
+        }
+
+    }
 }
 
 /// The relay thread handles all `ChatAction`s received on its MPSC channel
 /// by sending them out to all of the currently connected clients.
 fn relay_thread(/* TODO */) {
     // TODO
+//    for action in relay_mpsc_recv {
+//        // Send message to all clients.
+//    }
 }
 
 /// Each client thread waits for input (or disconnects) from its respective clients
@@ -42,6 +83,6 @@ fn relay_thread(/* TODO */) {
 ///
 /// * If the client sends any other message (i.e. `ChatAction::Msg`), it will be relayed verbatim.
 ///   (But you should still deserialize and reserialize the `ChatAction` to make sure it is valid!)
-fn client_thread(/* TODO */) {
+fn client_thread() {
     // TODO
 }
